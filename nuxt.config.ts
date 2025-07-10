6f8f99d03e20c4e5d67ae2f26bce7b76bf720b09
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-07-10 18:17:47 +0300

    refactor: clean up .prettierignore and format nuxt.config.ts for consistency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 /* global defineNuxtConfig */
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-  devtools: {enabled: true},
+  devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
     '@nuxtjs/seo',
@@ -14,9 +14,7 @@ export default defineNuxtConfig({
   ],
   css: ['~/assets/css/app.css'],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
   // Nuxt SEO: site-wide metadata configuration
   site: {
