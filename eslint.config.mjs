a031a868134fb5af4006e83d18b0d5bcb6d1a603
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-07-10 23:07:59 +0300

    docs: add missing documentation for ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 /**
  * ESLint configuration extended for Prettier integration.
+ *
  * - Uses eslint-plugin-prettier to run Prettier as an ESLint rule.
  * - Uses eslint-config-prettier to disable conflicting ESLint rules.
  *
