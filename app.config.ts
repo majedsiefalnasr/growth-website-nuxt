9668438170eb81a7ddc7bbe01d13e1e83fd7d354
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-07-14 16:36:14 +0300

    feat: add NuxtUI theme configuration for Growth platform

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+/**
+ * NuxtUI theme configuration for Growth platform
+ *
+ * This file customizes the NuxtUI container base class.
+ *
+ * @see https://ui.nuxt.com/getting-started/theming
+ */
+export default defineAppConfig({
+  ui: {},
+})
