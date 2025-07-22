5f82b85bcf6363dbc7cb6278f67f301098574ecf
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-07-22 12:42:15 +0300

    feat: add custom styles for icon component in app.css and configure icon size in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -103,7 +103,7 @@ export default defineNuxtConfig({
 
   // === Nuxt Icon module configuration ===
   icon: {
-    // size: '24px', // default <Icon> size applied
+    size: '24px', // default <Icon> size applied
     class: 'icon', // default <Icon> class applied
     mode: 'css', // default <Icon> mode applied
     cssLayer: 'base', // set the css layer to inject to
